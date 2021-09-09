f209a7315261c571f5ead19d0fb6bde0ea33823a
Sourabh Kumar <sourabh99967780@gmail.com>
2021-09-09 12:51:31 +0530

    Revision 5 more changes to the menu and some optimisations

--- assets/js/home.js
+++ assets/js/home.js
@@ -95,7 +95,7 @@ async function homePageOpeningAnimationPromise() {
   homePageOpeningAnimation
     .add({
       targets: ".left_img, .right_img",
-      duration: 5000,
+      duration: 3000,
       opacity: [0, 1],
     })
     .add({
