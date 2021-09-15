d51dba5c688960d88c187c85810b4a3e52ccaf72
Sourabh Kumar <sourabh99967780@gmail.com>
2021-09-15 11:06:56 +0530

    Ipad Idea work cut fix

--- assets/js/home.js
+++ assets/js/home.js
@@ -63,7 +63,7 @@ if (!isAMobileDevice) {
 }
 
 textWrapper.innerHTML =
-  '<span class="letter">G</span><span class="letter">r</span><span class="letter">e</span><span class="letter">a</span><span class="letter">t</span> <i><span class="letter">i</span><span class="letter">d</span><span class="letter">e</span><span class="letter">a</span></i> <span class="letter">i</span><span class="letter">s</span> <span class="letter">y</span><span class="letter">e</span><span class="letter">t</span> <span class="letter">t</span><span class="letter">o</span> <span class="letter">b</span><span class="letter">e</span>';
+  '<span class="letter">G</span><span class="letter">r</span><span class="letter">e</span><span class="letter">a</span><span class="letter">t</span> <i><span class="letter">idea</span></i> <span class="letter">i</span><span class="letter">s</span> <span class="letter">y</span><span class="letter">e</span><span class="letter">t</span> <span class="letter">t</span><span class="letter">o</span> <span class="letter">b</span><span class="letter">e</span>';
 
 async function homePageOpacityPromise() {
   var homePageOpacity = anime({
