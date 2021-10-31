524e63c9cbe80c0de217e9a149abac66c7f9f98b
Sourabh Kumar <sourabh99967880@gmail.com>
2021-10-31 13:49:14 +0530

    Sendgrid Email API Integration complete

--- assets/js/formHandler.js
+++ assets/js/formHandler.js
@@ -1,3 +1,5 @@
+const baseUrl = "http://localhost:3300/api";
+
 var vueApp = new Vue({
   el: "#contact-form",
   data() {
@@ -11,7 +13,6 @@ var vueApp = new Vue({
   },
   methods: {
     async saveToDatabase() {
-      const url = "http://localhost:3300/api/db/insert-enquiry";
       const data = {
         name: this.name,
         email: this.email,
@@ -19,7 +20,7 @@ var vueApp = new Vue({
         message: this.message,
       };
 
-      const response = await fetch(url, {
+      const response = await fetch(`${baseUrl}/db/insert-enquiry`, {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -35,7 +36,25 @@ var vueApp = new Vue({
     },
 
     async sendEmail() {
-      console.log("Email is sent");
+      const data = {
+        name: this.name,
+        email: this.email,
+        message: this.message,
+        phone: this.phone
+      }
+      const response = await fetch(`${baseUrl}/email`, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+      });
     },
 
     async handleFormData() {
