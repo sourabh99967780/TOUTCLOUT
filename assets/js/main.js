6e5ce6a06fea85a60b1770523274f7c5f7ea9c77
Sourabh Kumar <sourabhkumar@Sourabhs-MacBook-Air.local>
2022-04-18 22:16:39 +0530

    Database server changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,7 +178,7 @@ $('.enquireForm__tab').click(() => {
 
 // handle the function of enquire Form
 if (!isItContactUsPage && !isItTheHomePage) {
-  const baseUrl = "http://toutclout.abadboot.com/api";
+  const baseUrl = "http://toutclout.abadboot.com:3300/api";
   Vue.use(window.vuelidate.default);
   const { required, minLength, maxLength, email } = window.validators;
 
