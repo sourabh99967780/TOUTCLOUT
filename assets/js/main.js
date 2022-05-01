63aec6196d6c2d43a610c255031b95b92b148ca6
Sourabh Kumar <sourabhkumar@Sourabhs-MacBook-Air.local>
2022-05-01 17:07:27 +0530

    Unnecessary mobile code removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -176,30 +176,6 @@ $('.enquireForm__tab').click(() => {
   counter += 1;
 })
 
-// reomve all the hover state from mobile
-function hasTouch() {
-  return 'ontouchstart' in document.documentElement
-         || navigator.maxTouchPoints > 0
-         || navigator.msMaxTouchPoints > 0;
-}
-
-if (hasTouch()) { // remove all the :hover stylesheets
-  try { // prevent exception on browsers not supporting DOM styleSheets properly
-    for (var si in document.styleSheets) {
-      var styleSheet = document.styleSheets[si];
-      if (!styleSheet.rules) continue;
-
-      for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-        if (!styleSheet.rules[ri].selectorText) continue;
-
-        if (styleSheet.rules[ri].selectorText.match(':hover')) {
-          styleSheet.deleteRule(ri);
-        }
-      }
-    }
-  } catch (ex) {}
-}
-
 // handle the function of enquire Form
 if (!isItContactUsPage && !isItTheHomePage) {
   const baseUrl = "http://toutclout.abadboot.com:3300/api";
